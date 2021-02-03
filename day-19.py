0c6465c426be480415fe59d65bdb35305542fcca
AskSisyphus <75997124+AskSisyphus@users.noreply.github.com>
2021-02-03 18:03:51 -0500

    Update day-19.py

--- day-19.py
+++ day-19.py
@@ -28,7 +28,7 @@ while is_race_on:
     for turtle in all_turtles:
         if turtle.xcor() > 230:
             is_race_on = False
-            winning_color = turtle.pencolor
+            winning_color = turtle.pencolor()
             if winning_color == user_bet:
               print(f"You've won! The {winning_color} turtle is the winner! ")
             else:
